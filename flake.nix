3535831d3db8be44610a72cb91073bb250e44fea
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2025-11-12 22:05:17 +0100

    batman

---
+++ flake.nix
@@ -0,0 +1,102 @@
+{
+  description = "Starter Configuration for MacOS and NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager, nixpkgs, disko } @inputs:
+    let
+      user = "frbr";
+      darwinSystems = [ "aarch64-darwin" ];
+      devShell = system: let pkgs = nixpkgs.legacyPackages.${system}; in {
+        default = with pkgs; mkShell {
+          nativeBuildInputs = with pkgs; [ bashInteractive git ];
+          shellHook = with pkgs; ''
+            export EDITOR=vim
+          '';
+        };
+      };
+      mkApp = scriptName: system: {
+        type = "app";
+        program = "${(nixpkgs.legacyPackages.${system}.writeScriptBin scriptName ''
+          #!/usr/bin/env bash
+          PATH=${nixpkgs.legacyPackages.${system}.git}/bin:$PATH
+          echo "Running ${scriptName} for ${system}"
+          exec ${self}/apps/${system}/${scriptName}
+        '')}/bin/${scriptName}";
+      };
+      mkLinuxApps = system: {
+        "apply" = mkApp "apply" system;
+        "build-switch" = mkApp "build-switch" system;
+        "copy-keys" = mkApp "copy-keys" system;
+        "create-keys" = mkApp "create-keys" system;
+        "check-keys" = mkApp "check-keys" system;
+        "install" = mkApp "install" system;
+      };
+      mkDarwinApps = system: {
+        "apply" = mkApp "apply" system;
+        "build" = mkApp "build" system;
+        "build-switch" = mkApp "build-switch" system;
+        "copy-keys" = mkApp "copy-keys" system;
+        "create-keys" = mkApp "create-keys" system;
+        "check-keys" = mkApp "check-keys" system;
+        "rollback" = mkApp "rollback" system;
+      };
+    in
+    {
+      devShells = darwinSystems devShell;
+      apps = nixpkgs.lib.genAttrs darwinSystems mkDarwinApps;
+
+      darwinConfigurations = nixpkgs.lib.genAttrs darwinSystems (system: let
+        user = "frbr";
+      in
+        darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = inputs;
+          modules = [
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                inherit user;
+                enable = true;
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+                mutableTaps = false;
+                autoMigrate = true;
+              };
+            }
+            ./hosts/darwin
+          ];
+        }
+      );
+    };
+}
---
+++ modules/shared/packages.nix
@@ -0,0 +1,61 @@
+{ pkgs }:
+
+with pkgs; [
+  # General packages for development and system management
+  alacritty
+  aspell
+  aspellDicts.en
+  bash-completion
+  bat
+  btop
+  coreutils
+  killall
+  neofetch
+  openssh
+  sqlite
+  wget
+  zip
+  fzf
+  devenv
+  #direnv
+
+  # Encryption and security tools
+  age
+  age-plugin-yubikey
+  gnupg
+  libfido2
+
+  # Cloud-related tools and SDKs
+  docker
+  docker-compose
+
+  # Media-related packages
+  dejavu_fonts
+  ffmpeg
+  fd
+  font-awesome
+  hack-font
+  noto-fonts
+  noto-fonts-color-emoji
+  meslo-lgs-nf
+
+  # Node.js development tools
+  nodejs_24
+
+  # Text and terminal utilities
+  htop
+  hunspell
+  iftop
+  jetbrains-mono
+  jq
+  ripgrep
+  tree
+  tmux
+  unrar
+  unzip
+  zsh-powerlevel10k
+
+  # Python packages
+  python3
+  virtualenv
+]
